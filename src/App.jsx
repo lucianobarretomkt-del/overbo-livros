7cff82246b47ff5a44208b0868885de6a13cdbf0
Luciano Barreto <lucianobarretomkt@gmail.com>
2026-06-13 17:10:35 -0300

    fix: resolve white screen issue on Hostinger deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import JudgesPage from './pages/JudgesPage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/juizes" element={<JudgesPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
